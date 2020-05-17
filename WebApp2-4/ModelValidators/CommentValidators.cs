5233087db60abf112cac6834cd320534714de59a
Oana Raluca Pop <nrlcpp@gmail.com>
2020-05-17 12:17:45 +0300

    lab2 live..

---
+++ WebApp2-4/ModelValidators/CommentValidators.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp2_4.Models;
+
+namespace WebApp2_4.ModelValidators
+{
+    public class CommentValidators : AbstractValidator<Comment>
+    {
+        public CommentValidators(ExpensesDbContext _context)
+        {
+            RuleFor(x => x.Text).MinimumLength(2);
+        }
+    }
+}
--- WebApp2-4/Models/ExpensesDbContext.cs
+++ WebApp2-4/Models/ExpensesDbContext.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using System;
 
 namespace WebApp2_4.Models
 {
@@ -13,4 +13,24 @@ namespace WebApp2_4.Models
         public DbSet<Expense> Expenses { get; set; }
         public DbSet<Comment> Comments { get; set; }
     }
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+    //    modelBuilder.Entity<Expense>().HasData(
+    //    new Expense
+    //    {
+    //        Description = "First expense",
+    //        Sum = 120,
+    //        Location = "Auchan",
+    //        Date = new DateTime(2020, 03, 11),
+    //        Currency = "lei",
+    //        Type = Type.utilities
+
+    //    });
+          
+    //}
 }
+               
+  
+    
+
+
